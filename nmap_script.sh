a1d44ff2a99310bf8fa10e3d3bf4372aba7423be
secsalem <122979557+secsalem@users.noreply.github.com>
2024-09-29 12:53:00 +0400

    Update nmap_script.sh

--- nmap_script.sh
+++ nmap_script.sh
@@ -22,6 +22,9 @@ if [[ ! $ip_address =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     exit 1
 fi
 
+mkdir enum
+cd enum
+
 # Run the Nmap commands one by one and save output to files
 echo "Running Initial TCP Nmap scan..."
 nmap  -sV -oN initial_$ip_address.txt "$ip_address"
@@ -29,27 +32,48 @@ nmap  -sV -oN initial_$ip_address.txt "$ip_address"
 echo "Running Full TCP Nmap scan..."
 nmap  -sV -A -p- -oN full_$ip_address.txt "$ip_address"
 
-echo "Running Full UDP Nmap scan..."
-nmap -sU -p- -oN udp_$ip_address.txt "$ip_address"
+echo "Running dirsearch ..."
+dirsearch -u "$ip_address" -o dirsearch_$ip_address
 
 echo "Running Nmap NSE vulnerability scan..."
 nmap "$ip_address" --script vuln -oN vuln_$ip_address.txt
 
+echo "Running Nmap NSE vulnerability scan..."
+nmap "$ip_address" --script vuln -oN vuln_$ip_address.txt
+
+echo "Running Nikto web vulnerability scan..."
+nikto -h "$ip_address"  -o scan_results.txt -F txt -C all
+
+
+
 # Extract open ports from each scan file
 initial_ports=$(grep 'open' initial_$ip_address.txt | awk '{print $1}' | cut -d'/' -f1 | tr '\n' ',' | sed 's/,$//')
 full_tcp_ports=$(grep 'open' full_$ip_address.txt | awk '{print $1}' | cut -d'/' -f1 | tr '\n' ',' | sed 's/,$//')
-udp_ports=$(grep 'open' udp_$ip_address.txt | awk '{print $1}' | cut -d'/' -f1 | tr '\n' ',' | sed 's/,$//')
 
-# Combine all ports into a single variable, removing duplicates
-all_ports=$(echo "$initial_ports,$full_tcp_ports,$udp_ports" | tr ',' '\n' | sort -n | uniq | tr '\n' ',' | sed 's/,$//')
+
+# Combine all ports execluding udp , into a single variable, removing duplicates
+all_ports=$(echo "$initial_ports,$full_tcp_ports" | tr ',' '\n' | sort -n | uniq | tr '\n' ',' | sed 's/,$//')
 
 # Output the ports
 echo "Open ports: $all_ports"
 
 # Optionally, save the ports to a file
 echo "$all_ports" > open_ports_$ip_address.txt
-echo " -sC -A on all ports found"
-
 
 
 echo "All scans completed. Open ports saved to open_ports_$ip_address.txt , you can use these ports to further enumurate Ex: nmap -sC -A -p 22,80 etc "
+
+
+echo "Running Full UDP Nmap scan... will take some time"
+nmap -sU -p- -oN udp_$ip_address.txt "$ip_address"
+
+udp_ports=$(grep 'open' udp_$ip_address.txt | awk '{print $1}' | cut -d'/' -f1 | tr '\n' ',' | sed 's/,$//')
+
+# Combine udp ports and removing duplicates
+udp_ports=$(echo "$udp_ports" | tr ',' '\n' | sort -n | uniq | tr '\n' ',' | sed 's/,$//')
+
+# Output the UDP ports
+echo "Open UDP ports: $udp_ports"
+
+# Optionally, save the ports to a file
+echo "$udp_ports" > open_udp_ports_$ip_address.txt
